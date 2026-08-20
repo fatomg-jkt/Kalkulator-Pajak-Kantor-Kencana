9346275193448311bf99abc97a3a5f66cab8095d
fatomg-jkt <fat@obsidianmanagement-group.com>
2026-08-20 17:16:19 +0700

    Trigger Vercel preview after Git connection

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import TaxCalculator from '../components/TaxCalculatorPreviewV3';
 
+// Preview entrypoint: regulation cards link to official government sources.
 export default function Home() {
   return <TaxCalculator />;
 }
