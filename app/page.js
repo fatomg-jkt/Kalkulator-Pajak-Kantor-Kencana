f551343ff1ad1e08e878ea9e08f6fda677dd27b7
fatomg-jkt <fat@obsidianmanagement-group.com>
2026-08-27 16:39:32 +0700

    Use expanded preview tax calculator

--- app/page.js
+++ app/page.js
@@ -1,4 +1,4 @@
-import TaxCalculator from '../components/TaxCalculatorPreviewV6';
+import TaxCalculator from '../components/TaxCalculatorPreviewV7';
 
 export default function Home() {
   return <TaxCalculator />;
